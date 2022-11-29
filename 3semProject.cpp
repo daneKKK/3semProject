fee0ca5c1eb23c1382b4d1bb9d106e8c04117cf2
Daniil Alekseev <daniaalekseew@yandex.ru>
2022-11-29 16:09:16 +0300

    Добавьте файлы проекта.

---
+++ 3semProject.cpp
@@ -0,0 +1,12 @@
+﻿// 3semProject.cpp: определяет точку входа для приложения.
+//
+
+#include "3semProject.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
